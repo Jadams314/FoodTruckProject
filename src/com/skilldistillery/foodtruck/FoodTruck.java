fdd2c0d37986ceb362246972f1bcf6e4f98ea1f3
jadams314 <justin.adams314@gmail.com>
2021-01-23 14:32:45 -0600

    built the bones

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,45 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+	public String name;
+	public String foodType;
+	public double rating;
+	public int truckId;
+/* When a FoodTruck is created, its constructor assigns its 
+ * id field the current value of a static field (such as (nextTruckId) 
+ * and then increments the static field.
+ */
+	
+	public FoodTruck() {}
 
+	public FoodTruck(String name, String foodType, double rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.truckId = truckId++;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public int getTruckId() {
+		return truckId;
+	}
+	public double getAverage() {
+		return 0.0;
+	}
 }
--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,10 +1,108 @@
 package com.skilldistillery.foodtruck;
 
+import java.util.Scanner;
+
+
 public class FoodTruckApp {
+private FoodTruckFleet fleet = new FoodTruckFleet();
+static Scanner kb = new Scanner(System.in);
+	/*User Story #1
+	 *	The user is prompted to input the name, food type, and rating for up to five food trucks.
+	 *	 For each set of input, a FoodTruck object is created, its fields set to the user's input,
+	 *	  and it is added to the array. The truck id is not input by the user, but instead assigned 
+	 *	  automatically in the FoodTruck constructor from a static field that is incremented as each 
+	 *    truck is created.
+	 */	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello Food Truck");
+// make user method to build food truck..... will ask the user for the name,food type and their rating
+	// after each truck ask if they would like to add another. they must be able to quit out of adding 
+	// the truck during the name phase by typing quit.
+	
+
+/* User Story #3
+	After input is complete, the user sees a menu from which they can choose to:
+
+	List all existing food trucks.
+	See the average rating of food trucks.
+	Display the highest-rated food truck.
+	Quit the program.
+ * 	
+ */
+	
+// make print menu method with a switch
+	// think about adding another button to add another food truck if the user hasn't met the max entry's
+		// whether or not I add the option to add more trucks I need to have a counter for the number
+		// of trucks the user has added.
+//	for (int i = 0; i < fTruck.length; i++) {
+//		
+//	}
+	public FoodTruck buildTruck() {				// getting truck info to send to FoodTruck()
+		
+			System.out.println("What is the name of the food truck?");
+			String	name = kb.next();
+			System.out.println("What type of food does " + name + " have?");
+			String	type = kb.next();
+			System.out.println("On a scale of 1-5, how would you rate the food at " + name);
+			double	rating = kb.nextDouble();
+
+			FoodTruck fTruck = new FoodTruck (name, type, rating);
+			return fTruck;
 	}
-
+	public void addTruckToFleet(FoodTruck fTruck) {
+		fleet.addTruck(fTruck);
+		System.out.println("food Truck added");
+	}
+	public FoodTruck [] printTrucks() {
+		FoodTruck [] trucksInFleet = fleet.getTruck();
+		
+		return trucksInFleet;
+	}
+	public void avgRating() {
+		
+	}
+	public void highestRated() {
+		
+	}
+	
+	public void printMenu() {
+		System.out.println("Welcome to the Menu");
+		System.out.println();
+		System.out.println("1) List all existing food trucks ");
+		System.out.println("2) See the average rating of food trucks ");
+		System.out.println("3) Disply the highest rated food truck ");
+		System.out.println("4) Quit the program ");
+		menuSwitch();
+	}
+	
+	public void menuSwitch() {
+		System.out.println("What would you like to do?");
+		int result = kb.nextInt();
+		
+		switch (result) {
+		case 1:
+			System.out.println("Ok, here is the list of trucks stored");
+			System.out.println( fleet.truck[0] + " " );
+			break;
+		case 2:
+			avgRating();
+			break;
+		case 3:
+			highestRated();
+			break;
+		case 4:
+			System.out.println("You have chosen to quit");
+			System.out.println("	Goode Bye");
+			System.exit(result);
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		FoodTruckApp user = new FoodTruckApp();
+		
+		user.buildTruck();
+		user.printMenu();
+		kb.close();
+	}
 }
